c2130cf7566669ab38c7554e270d13cad95bc42f
retra <jozalamun@gmail.com>
2024-07-18 11:05:11 -0400

    Write tests for and add Gameboard class/methods

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Ship {
   }
 
   hit() {
-    this.hitCount += 1;
+    this.hitCount++;
   }
 
   isSunk() {
@@ -19,4 +19,71 @@ class Ship {
   }
 }
 
-export { Ship };
+class Gameboard {
+  constructor() {
+    this.board = this.createBoard();
+    this.ships = [];
+    this.missedShots = [];
+  }
+
+  createBoard() {
+    const boardArr = [];
+    const size = 10;
+    for (let i = 0; i < size; i++) {
+      let row = [];
+      for (let j = 0; j < size; j++) {
+        row.push("-");
+      }
+      boardArr.push(row);
+    }
+    return boardArr;
+  }
+
+  addShips() {
+    const shipsArr = [];
+    shipsArr.push(new Ship(2));
+    shipsArr.push(new Ship(3));
+    shipsArr.push(new Ship(3));
+    shipsArr.push(new Ship(4));
+    shipsArr.push(new Ship(5));
+    return shipsArr;
+  }
+
+  placeShip(ship, xCoordinate, yCoordinate, orientation = "horizontal") {
+    for (let i = 0; i < ship.length; i++) {
+      if (orientation === "vertical") {
+        this.board[xCoordinate][yCoordinate + i] = ship;
+      } else {
+        this.board[xCoordinate + i][yCoordinate] = ship;
+      }
+      this.ships.push(ship);
+    }
+  }
+
+  receiveAttack(xCoordinate, yCoordinate) {
+    if (this.board[xCoordinate][yCoordinate] instanceof Ship) {
+      this.board[xCoordinate][yCoordinate].hit();
+      return true;
+    } else {
+      this.missedShots.push([xCoordinate, yCoordinate]);
+      return false;
+    }
+  }
+
+  allSunk() {
+    let sunkCount = 0;
+    this.ships.forEach((ship) => {
+      if (ship.isSunk()) {
+        sunkCount++;
+      }
+      sunkCount;
+    });
+    if (sunkCount === this.ships.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export { Ship, Gameboard };
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { Ship } from "./index";
+import { Ship, Gameboard } from "./index";
 
 describe("Ship methods", () => {
   let newShip;
@@ -38,5 +38,140 @@ describe("Ship methods", () => {
     newShip.hit();
     newShip.hit();
     expect(newShip.isSunk()).toBe(true);
+  });
+});
+
+describe("Gameboard methods", () => {
+  let newGameboard;
+  let x;
+  let y;
+  let newShip;
+
+  describe("Gameboard.place method", () => {
+    beforeEach(() => {
+      newGameboard = new Gameboard();
+      x = 3;
+      y = 4;
+      newShip = new Ship(3);
+    });
+
+    test("only coords where ship was placed should be a Ship object", () => {
+      newGameboard.placeShip(newShip, x, y);
+      expect(newGameboard.board[x][y]).toBeInstanceOf(Ship);
+      expect(newGameboard.board[1][1]).not.toBeInstanceOf(Ship);
+    });
+
+    describe("when orientation argument is horizontal", () => {
+      beforeEach(() => {
+        newGameboard.placeShip(newShip, x, y);
+      });
+
+      test("Cell horizontally next to first cell should be the same ship object", () => {
+        expect(newGameboard.board[x + 1][y]).toBe(newShip);
+      });
+
+      test("Cell vertically next to first cell should NOT be the same ship object", () => {
+        expect(newGameboard.board[x][y + 1]).not.toBe(newShip);
+      });
+    });
+
+    describe("when orientation argument is vertical", () => {
+      beforeEach(() => {
+        newGameboard.placeShip(newShip, x, y, "vertical");
+      });
+      test("Cell vertically next to first cell should be the same ship object", () => {
+        expect(newGameboard.board[x][y + 2]).toBe(newShip);
+      });
+
+      test("Cell horizontally next to first cell should NOT be the same ship object", () => {
+        expect(newGameboard.board[x + 1][y]).not.toBe(newShip);
+      });
+    });
+  });
+
+  describe("receiveAttack method", () => {
+    beforeEach(() => {
+      newGameboard = new Gameboard();
+      x = 3;
+      y = 4;
+      newShip = new Ship(3);
+      newGameboard.placeShip(newShip, x, y);
+    });
+
+    describe("when receiveAttack coordinates are a ship", () => {
+      test("returns true", () => {
+        expect(newGameboard.receiveAttack(x + 1, y)).toBe(true);
+      });
+
+      test("calls hit method", () => {
+        const spy = jest.spyOn(newShip, "hit");
+
+        newGameboard.receiveAttack(x + 1, y);
+
+        // calls hit when hit
+        expect(spy).toHaveBeenCalled();
+
+        spy.mockRestore();
+      });
+    });
+
+    describe("when attack misses", () => {
+      test("returns false", () => {
+        expect(newGameboard.receiveAttack(x, y + 1)).toBe(false);
+      });
+
+      test("does not call hit method", () => {
+        const spy = jest.spyOn(newShip, "hit");
+
+        newGameboard.receiveAttack(x, y + 1);
+
+        expect(spy).not.toHaveBeenCalled();
+
+        spy.mockRestore();
+      });
+
+      test("records the coordinates of the missed shot", () => {
+        newGameboard.receiveAttack(x, y + 1);
+
+        expect(newGameboard.missedShots.at(-1)).toEqual([x, y + 1]);
+      });
+    });
+  });
+
+  describe("allSunk method", () => {
+    let newX;
+    let newY;
+    let newShip2;
+
+    beforeEach(() => {
+      newGameboard = new Gameboard();
+      x = 3;
+      y = 4;
+      newShip = new Ship(2);
+      newGameboard.placeShip(newShip, x, y);
+      newX = 2;
+      newY = 5;
+      newShip2 = new Ship(3);
+
+      newGameboard.placeShip(newShip, x, y);
+      newGameboard.placeShip(newShip2, newX, newY, "vertical");
+
+      newGameboard.receiveAttack(x, y);
+      newGameboard.receiveAttack(x + 1, y);
+    });
+
+    test("returns true when all ships are sunk", () => {
+      newGameboard.receiveAttack(newX, newY);
+      newGameboard.receiveAttack(newX, newY + 1);
+      newGameboard.receiveAttack(newX, newY + 2);
+
+      expect(newGameboard.allSunk()).toBe(true);
+    });
+
+    test("returns false when not all ships are sunk", () => {
+      newGameboard.receiveAttack(newX, newY);
+
+      expect(newGameboard.allSunk()).toBe(false);
+    });
   });
 });
